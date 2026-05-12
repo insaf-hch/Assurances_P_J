a96cfb94deb88fce1aafb59c610b5bfc74fd196f
Insaf Hammouchi <insafhammouchi28@gmail.com>
2026-05-12 23:13:53 +0200

    fix: hardcode port 8080 nginx

--- docker/start.sh
+++ docker/start.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-export PORT=${PORT:-8080}
-
-echo "=== PORT is: $PORT ==="
-
-cp /etc/nginx/sites-available/default.template /etc/nginx/sites-available/default
-sed -i "s/\${PORT}/$PORT/g" /etc/nginx/sites-available/default
-ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-
 echo "=== Starting php-fpm ==="
 /usr/local/sbin/php-fpm -D
 sleep 3
+
+echo "=== Copying nginx config ==="
+cp /etc/nginx/sites-available/default.template /etc/nginx/sites-available/default
+ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
 echo "=== Starting nginx ==="
 exec nginx -g "daemon off;"
