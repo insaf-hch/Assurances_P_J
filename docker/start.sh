cc2cdc5ed7546540d5d2c65a04f28b4ce1d46fa5
Insaf Hammouchi <insafhammouchi28@gmail.com>
2026-05-12 22:28:02 +0200

    fix: use full path for php-fpm

--- docker/start.sh
+++ docker/start.sh
@@ -7,11 +7,9 @@ cp /etc/nginx/sites-available/default.template /etc/nginx/sites-available/defaul
 sed -i "s/\${PORT}/$PORT/g" /etc/nginx/sites-available/default
 ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
-echo "=== nginx config ==="
-cat /etc/nginx/sites-available/default
+echo "=== Starting php-fpm ==="
+/usr/local/sbin/php-fpm -D
+sleep 3
 
-php-fpm -D
-sleep 2
-
-echo "=== starting nginx ==="
+echo "=== Starting nginx ==="
 exec nginx -g "daemon off;"
