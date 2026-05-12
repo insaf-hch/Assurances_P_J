ecda5f6df4299b8ae4749c2d01f753cd47c1e019
Insaf Hammouchi <insafhammouchi28@gmail.com>
2026-05-12 23:41:59 +0200

    debug: show laravel logs

--- docker/start.sh
+++ docker/start.sh
@@ -3,15 +3,17 @@ echo "=== Starting php-fpm ==="
 /usr/local/sbin/php-fpm -D
 sleep 3
 
+echo "=== Setting up .env ==="
+cd /var/www/html
+cp .env.example .env
+php artisan key:generate --force
+
 echo "=== Copying nginx config ==="
 cp /etc/nginx/sites-available/default.template /etc/nginx/sites-available/default
 ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
 echo "=== Running migrations ==="
-cd /var/www/html && php artisan migrate --force 2>&1
-
-echo "=== Laravel logs ==="
-tail -f /var/www/html/storage/logs/laravel.log &
+php artisan migrate --force 2>&1
 
 echo "=== Starting nginx ==="
 exec nginx -g "daemon off;"
