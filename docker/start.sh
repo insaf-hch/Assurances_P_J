8117018c886cafab27edc4479475a6c45c82ba18
Insaf Hammouchi <insafhammouchi28@gmail.com>
2026-05-13 10:22:48 +0200

    fix: parse MYSQL_URL with awk

--- docker/start.sh
+++ docker/start.sh
@@ -6,7 +6,18 @@ sleep 3
 echo "=== Setting up .env ==="
 cd /var/www/html
 
-cat > .env << EOF
+# Extraire depuis MYSQL_URL avec awk (plus fiable que sed)
+PARSED_USER=$(echo "$MYSQL_URL" | awk -F'[@/:]' '{print $4}')
+PARSED_PASS=$(echo "$MYSQL_URL" | awk -F'@' '{print $1}' | awk -F'://' '{print $2}' | awk -F':' '{print $2}')
+PARSED_HOST=$(echo "$MYSQL_URL" | awk -F'@' '{print $2}' | awk -F':' '{print $1}')
+PARSED_PORT=$(echo "$MYSQL_URL" | awk -F'@' '{print $2}' | awk -F'[:/]' '{print $2}')
+PARSED_DB=$(echo "$MYSQL_URL" | awk -F'/' '{print $NF}')
+
+echo "HOST=$PARSED_HOST"
+echo "PORT=$PARSED_PORT"
+echo "DB=$PARSED_DB"
+
+cat > .env << ENVEOF
 APP_NAME=Laravel
 APP_ENV=production
 APP_KEY=${APP_KEY}
@@ -14,18 +25,18 @@ APP_DEBUG=false
 APP_URL=${APP_URL}
 
 DB_CONNECTION=mysql
-DB_HOST=${MYSQLHOST}
-DB_PORT=${MYSQLPORT}
-DB_DATABASE=${MYSQLDATABASE}
-DB_USERNAME=${MYSQLUSER}
-DB_PASSWORD=${MYSQLPASSWORD}
+DB_HOST=${PARSED_HOST}
+DB_PORT=${PARSED_PORT}
+DB_DATABASE=${PARSED_DB}
+DB_USERNAME=${PARSED_USER}
+DB_PASSWORD=${PARSED_PASS}
 
 SESSION_DRIVER=database
 CACHE_STORE=database
 QUEUE_CONNECTION=database
 LOG_CHANNEL=stderr
 LOG_LEVEL=debug
-EOF
+ENVEOF
 
 echo "=== Copying nginx config ==="
 cp /etc/nginx/sites-available/default.template /etc/nginx/sites-available/default
