cf2b28d244388b6edd20759609571cef24c84afd
Insaf Hammouchi <insafhammouchi28@gmail.com>
2026-05-18 12:45:01 +0200

    feat: nouvelle fonctionnalité

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,8 +19,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-   public function boot(): void
-    {
-        Schema::defaultStringLength(191); // ✅ LA SOLUTION
-    }
+
+
+public function boot(): void
+{
+    URL::forceScheme('https');
+}
 }
